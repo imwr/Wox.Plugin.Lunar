732824976fea3f2f55be6438525ad123116326a9
王锐 <mvpjly@163.com>
2016-09-05 17:47:12 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Wox.Plugin.Lunar
-a qrcode lunar plugin of wox
+一个公历Wox插件
+
+## Features
+* 支持所有标准的日期、时间格式的字符串
+* 可查询农历、节气，以及节日等信息
+
+## Usage
+* 在windows上安装 [wox][1] , 然后按 `Alt+Space` 启动
+* 输入 `wpm install lunar`安装此插件
+* 输入`nl` 激活使用
+
+## Syntax
+Standard Date and Time Format Strings, eg:
+
+ - `nl`
+ - `nl Sep`
+ - `nl 15`
+ - `nl 10:11:12`
+ - `nl 2016-09-15`
+ - `nl Sep of 16`
+ - `nl 201609151011`
+ - `nl 2016-09-15T10:11:12`
+ - `nl Thu Sep 15 10:11:12 2016`
+
+## Screenshot
+![image](Images/screenshot.png)
+
+[1]: http://www.getwox.com/
---
+++ main.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from wox import Wox
+from lunar import Lunar
+from dateutil.parser import parse
+
+
+class ChineseCalendar(Wox):
+    def query(self, query):
+        result = []
+        try:
+            if not query:
+                ln = Lunar()
+            else:
+                ln = Lunar(parse(query))
+        except ValueError:
+            result.append({
+                "Title": 'Can not parse input string',
+                "SubTitle": 'Please read the docs',
+                "IcoPath": "Images/app.png",
+            })
+        else:
+            jieqi = ln.ln_jieqi()
+            jieri_lu = ln.ln_jie(True)
+            jieri = ln.ln_jie()
+            date = ln.localtime.strftime('%Y-%m-%d %H:%M:%S')
+            notime = False
+            if date[11:] == "00:00:00":
+                notime = True
+            result.append({
+                "Title": '时间 {} {}'.format(date[0: 11] if notime else date,
+                                           '【' + jieri + '】' if jieri else ""),
+                "SubTitle": '{} 【{}】 【{}年-{}日】 {} {} {}'.format(ln.ln_date_str(), ln.gz_year(), ln.sx_year(),
+                                                                ln.gz_day(),
+                                                                (ln.gz_hour() + "时") if notime else "",
+                                                                '【' + jieqi + '】' if jieqi else "",
+                                                                '【' + jieri_lu + '】' if jieri_lu else ""),
+                "IcoPath": "Images/app.png",
+            })
+
+            return result
+
+
+if __name__ == "__main__":
+    ChineseCalendar()
